ffd124c6e252b6eaf97a04aa5f84f0dd9b796959
vincentutama <41050751+vincentutama@users.noreply.github.com>
2020-06-07 02:55:40 +0700

    Updates

---
+++ precache-manifest.6f0024dc19af3a6aa261d0be384bc454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81aa34ff87220104efab630ab7476124",
+    "url": "/progweb-portfolio/index.html"
+  },
+  {
+    "revision": "ca01d84ef3a4c6669684",
+    "url": "/progweb-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dfe4725259ce2d199fa0",
+    "url": "/progweb-portfolio/static/js/2.40114c41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/progweb-portfolio/static/js/2.40114c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca01d84ef3a4c6669684",
+    "url": "/progweb-portfolio/static/js/main.8b491aa8.chunk.js"
+  },
+  {
+    "revision": "2f591e6d8dfbd3a9611b",
+    "url": "/progweb-portfolio/static/js/runtime-main.815962ba.js"
+  }
+]);
